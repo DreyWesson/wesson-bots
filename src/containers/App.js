8ab1363d713efe3034fba6098923d5a610431cc1
dreyWesson <dreyWesson@gmail.com>
2020-03-24 23:16:43 +0100

    Setting up state with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorHandler from "../components/ErrorHandler";
 import "./App.css";
+import { setSearchField } from "../actions";
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchField: ""
+      robots: []
     };
   }
 
@@ -20,22 +33,18 @@ class App extends Component {
       .then(users => this.setState({ robots: users }));
   }
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
-
   render() {
-    const { robots, searchField } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-
     return !robots.length ? (
-      <h1 className="red">Loading...</h1>
+      <h1>Loading...</h1>
     ) : (
       <div className="tc">
         <h1 className="f2">Wesson-bots</h1>
-        <SearchBox onSearch={this.onSearchChange} />
+        <SearchBox onSearch={onSearchChange} />
         <br />
         <Scroll>
           <br />
@@ -48,4 +57,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
