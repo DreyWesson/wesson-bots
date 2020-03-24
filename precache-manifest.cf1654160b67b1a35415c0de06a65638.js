87c4e674a7e153b120325d55b10af5680a04c3f3
dreyWesson <dreyWesson@gmail.com>
2020-03-24 15:53:26 +0100

    Updates

---
+++ precache-manifest.cf1654160b67b1a35415c0de06a65638.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84af3d230ee3d7668e7fb9401969c194",
+    "url": "/wesson-bots/index.html"
+  },
+  {
+    "revision": "e81f281830593a42f481",
+    "url": "/wesson-bots/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "04b4ad3561be8c779b19",
+    "url": "/wesson-bots/static/css/main.88dba61a.chunk.css"
+  },
+  {
+    "revision": "e81f281830593a42f481",
+    "url": "/wesson-bots/static/js/2.b0fbf7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wesson-bots/static/js/2.b0fbf7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b4ad3561be8c779b19",
+    "url": "/wesson-bots/static/js/main.4d884dd0.chunk.js"
+  },
+  {
+    "revision": "8eb936bf3d38ef62875d",
+    "url": "/wesson-bots/static/js/runtime-main.7b875e67.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/wesson-bots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
